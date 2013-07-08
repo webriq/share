981c0ff34691695eb011a6496e6828b2314fc47d
David Pozsar <david.pozsar@megaweb.hu>
2013-07-08 12:34:40 +0200

    fix missing styles & email typo

--- public/scripts/zork/share/email.js
+++ public/scripts/zork/share/email.js
@@ -18,44 +18,45 @@
      */
     global.Zork.Share.prototype.email = function ( element )
     {
-        js.style('/styles/modules/Share/email.css');
-
+        js.style( "/styles/modules/Share/email.css" );
         element = $( element );
 
-        var backendUrl = '/app/' + element.attr('data-locale') + '/share/email';
+        var backendUrl = "/app/" + element.data( "locale" ) + "/share/email";
 
-        if(element[0].tagName.toLowerCase()=='a')
+        if ( element.is( "a" ) )
         {
-            element.addClass('share-button-email');
-            element.css(
-            {
-                display: 'block',
-                position: 'relative',
-                top: 0,
-                left: 0,
-                height: '14px',
-                lineHeight: '14px',
-                fontSize: '11px',
-                fontFamily: 'Arial',
-                padding: '2px 5px 2px 25px',
-                margin: '0 5px 0 0',
-                border: 'solid 1px #cacaca',
-                background: 'url(/images/modules/Share/email_16x16.png) no-repeat scroll 3px center #F2F2F2',
-                textDecoration: 'none',
-                color: '#555555',
-                fontWeight: 'bold',
-                '-moz-border-radius': '3px',
-                'border-radius': '3px'
-            });
+            element.addClass( "share-button-email" )
+                   .css( {
+                        "display": "inline-block",
+                        "position": "relative",
+                        "top": 0,
+                        "left": 0,
+                        "height": "14px",
+                        "line-height": "14px",
+                        "font-size": "11px",
+                        "font-family": "Arial",
+                        "padding": "2px 5px 2px 25px",
+                        "margin": "0 5px 0 0",
+                        "border": "1px solid #cacaca",
+                        "background": 'url("/images/modules/Share/email_16x16.png") no-repeat scroll 3px center #F2F2F2',
+                        "text-decoration": "none",
+                        "color": "#555555",
+                        "font-weight": "bold",
+                        "-moz-border-radius": "3px",
+                        "border-radius": "3px"
+                    } );
 
-            element.on('click',function() {
-                var dialog = null ,
+            element.on( "click", function() {
+                var dialog = null,
                     dialogClose = null,
                     dialogShow = function(content)
                     {
                         var id  = "js-share-email-" + js.generateId.number();
 
-                        if( Function.isFunction(dialogClose) ){ dialogClose(); }
+                        if ( Function.isFunction( dialogClose ) )
+                        {
+                            dialogClose();
+                        }
 
                         dialog  =
                             $( '<div id="' + id + '" name="' + id + '" />' )
@@ -68,30 +69,31 @@
                                 "background-color"  : "#ffffff"
                             } )
                             .append(
-                                $('<div/>')
-                                .css('min-height','100px')
-                                .css('background','#fff url(/images/scripts/loading.gif) no-repeat center center')
+                                $( "<div />" )
+                                    .css( {
+                                        "min-height": "100px",
+                                        "background": '#fff url("/images/scripts/loading.gif") no-repeat center center'
+                                    } )
                             )
                             .html( content );
 
-                        js.core.parseDocument(dialog);
+                        js.core.parseDocument( dialog );
 
-                        var form = dialog.find('form');
+                        var form = dialog.find( "form" );
 
-                        if( form[0] )
+                        if ( form.length )
                         {
-                            form.find('input[name=cancel]')
-                                .on('click',function(){ dialogClose(); return false } );
-                            form.on('submit',submitEvent);
+                            form.find( "input[name=cancel]" )
+                                .on( "click", function () { dialogClose(); return false; } );
+                            form.on( "submit", submitEvent);
                         }
                         else
                         {
-                            dialog
-                                .find('button.result-close')
-                                .on('click',function(){ dialogClose(); });
-                            setTimeout(function(){ dialogClose(); },5000);
-
+                            dialog.find( "button.result-close" )
+                                  .on( "click", function () { dialogClose(); } );
+                            setTimeout( function () { dialogClose(); }, 5000 );
                         }
+
                         dialogClose = js.core.layer( dialog );
 
                     },//end of dialogShow
@@ -99,29 +101,31 @@
                     {
                         var form =  $(this);
 
-                        dialog.children().css('background','#fff url(/images/scripts/loading.gif) no-repeat center center').children().css('opacity','0.0');
+                        dialog.children()
+                              .css( "background", '#fff url("/images/scripts/loading.gif") no-repeat center center' )
+                              .children()
+                              .css( "opacity", "0.0" );
 
-                        $.post(backendUrl,form.serializeArray(),function(result)
-                        {
-                            dialogShow(result);
-                        });
+                        $.post( backendUrl, form.serializeArray(), function ( result ) {
+                            dialogShow( result );
+                        } );
 
                         return false;
                     }//end of submitEvent
                 ;
 
-                $.ajax(
-                {
-                    url: backendUrl,
-                    data: {"shareUrl": element.attr('data-share-url')}
-                })
-                .done( function ( result )
-                {
-                    dialogShow(result);
-                });
+                $.ajax( {
+                    "url": backendUrl,
+                    "data": {
+                        "shareUrl": element.data( "shareUrl" )
+                    }
+                } )
+                .done( function ( result ) {
+                    dialogShow( result );
+                } );
 
                 return false;
-            });
+            } );
 
         }
     };
