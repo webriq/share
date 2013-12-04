7d1403022290148d5bf580120e44338f0741f5da
David Pozsar <david.pozsar@megaweb>
2013-12-04 13:43:11 +0100

    facebook changes

--- public/scripts/zork/share/facebook.js
+++ public/scripts/zork/share/facebook.js
@@ -13,28 +13,9 @@
         return;
     }
 
-    var fbInited = false,
-        fbInit = function ( locale ) {
-            $('body').prepend('<div id="fb-root"></div>');
-            $.getScript('http://connect.facebook.net/'+locale+'/all.js', function() {
-                global.FB.init({status: true, cookie: true, xfbml: true});
-            });
-        };
-
     global.Zork.Share.prototype.facebook = function ( element )
     {
-        element = $( element );
-
-        if ( ! fbInited )
-        {
-            fbInited = true;
-            fbInit( element.data('locale') );
-        }
-
-        if(typeof(global.FB)!='undefined' && typeof(global.FB.XFBML)!='undefined' && typeof(global.FB.XFBML.parse)=='function')
-        {
-            global.FB.init({status: true, cookie: true, xfbml: true});
-        }
+        js.require( 'js.facebook' ).xfbml( element );
     };
 
     global.Zork.Share.prototype.facebook.isElementConstructor = true;
--- src/Grid/Share/View/Helper/Button/Facebook.php
+++ src/Grid/Share/View/Helper/Button/Facebook.php
@@ -4,7 +4,7 @@ namespace Grid\Share\View\Helper\Button;
 use Zend\View\Helper\AbstractHelper;
 
 /**
- * Share button viewHelper  
+ * Share button viewHelper
  *
  * @author Kristof Matos <kristof.matos@megaweb.hu>
  */
@@ -12,22 +12,23 @@ class Facebook extends AbstractHelper implements ButtonInterface
 {
    /**
      * Renders button html
-     * 
+     *
      * @param string $url
      * @param string $locale
      * @return string
      */
     public function getHtml( $url, $locale )
-    {      
+    {
         $url = htmlspecialchars($url);
         return <<<SCRIPT
-<div data-locale="$locale" data-js-type="js.share.facebook" 
-        class="fb-like" 
-        data-href="$url" 
-        data-send="false" 
-        data-width="100" 
-        data-layout="button_count"
-        data-show-faces="false"></div>
+<div data-locale="$locale"
+     data-js-type="js.facebook.xfbml"
+     class="fb-like"
+     data-href="$url"
+     data-send="false"
+     data-width="100"
+     data-layout="button_count"
+     data-show-faces="false"></div>
 SCRIPT;
     }
 
